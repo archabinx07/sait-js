9c21cb4b14d118f06d3a7822db405ae2b7fcb290
Wilmar Delgado <791879@ACDM.DS.SAIT.CA>
2018-10-09 10:03:31 -0600

    ok

--- Day1/mycode.js
+++ Day1/mycode.js
@@ -1,4 +1,27 @@
 alert('Hello World!'); // Displays and alert
 console.log('Secret Messsage'); // Lets you do secret staff
+
 var num = 5;
 console.log(num);
+
+var numOfPuppies = 5;
+var numOfKittens = 7;
+var animalsLeft = numOfPuppies - numOfKittens;
+var numOfCats = numOfKittens + 2;
+console.log("number of cats: ", numOfCats);
+
+var alphabet = "abcdefghijkl";
+console.log("letters: ",alphabet);
+
+var fullName = "Wilmar Delgado";
+console.log("Full Name: ",fullName);
+
+var fullName = "Wilmar ";
+fullName += "Delgado";
+console.log(fullName); //Outputs Full Name
+
+var name = prompt('Whats your name?')
+console.log("hello " + name + " welcome back!");
+
+
+
